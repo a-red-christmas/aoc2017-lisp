f6ed17b8b2429b66415e92295557107ef4c8883f
Eugene Zaikonnikov <eugene@funcall.org>
2017-12-10 22:29:08 +0100

    day 10

--- 10.lisp
+++ 10.lisp
@@ -13,16 +13,15 @@
 
 (defun encode2 (input)
   (with-open-file (s input :direction :input)
-    (let* ((cypher (append (loop for c across (read-line s) collecting (char-code c)) '(17 31 73 47 23)))
-	   (wheel (loop for i from 0 to 255 collecting i))
-	   (cursor (setf (cdr (last wheel)) wheel))
-	   (skip 0))
-      (loop repeat 64 do
-	   (loop for len in cypher do
-		(loop with reverse = (nreverse (loop repeat len for e in cursor collecting e))
-		   for i from 0 below len do (setf (nth i cursor) (pop reverse)))
-		(setf cursor (nthcdr (+ len skip) cursor))
-		(incf skip))
-	 finally (return (format nil "~{~2,'0x~}"
-				 (loop repeat 16 collecting
-				      (reduce #'logxor (loop repeat 16 collecting (pop wheel))))))))))
+    (loop repeat 64
+       with cypher = (append (loop for c across (read-line s) collecting (char-code c)) '(17 31 73 47 23))
+       and wheel = (loop for i from 0 to 255 collecting i)
+       with cursor = (setf (cdr (last wheel)) wheel) and skip = 0 do
+	 (loop for len in cypher do
+	      (loop with reverse = (nreverse (loop repeat len for e in cursor collecting e))
+		 for i from 0 below len do (setf (nth i cursor) (pop reverse)))
+	      (setf cursor (nthcdr (+ len skip) cursor))
+	      (incf skip))
+       finally (return (format nil "~{~2,'0x~}"
+			       (loop repeat 16 collecting
+				    (reduce #'logxor (loop repeat 16 collecting (pop wheel)))))))))
