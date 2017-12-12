18a7fa4d3208bb6662eaae579ca601c56c551d40
Eugene Zaikonnikov <eugene@funcall.org>
2017-12-12 22:37:33 +0100

    day 12

--- 12.lisp
+++ 12.lisp
@@ -2,6 +2,17 @@
 
 (defun components (input)
   (with-open-file (s input :direction :input)
-    (loop for line = (read-from-string (read-line s nil)) while line
-       collecting (cons (first line) (nthcdr 2 line))) with components = '(0)
-       when (member ) )))
+    (loop with records = (loop for line = (read-line s nil) while line
+			    for edge = (read-from-string (format nil "(~A)" (nsubstitute #\Space #\, line))) collecting (nthcdr 2 edge))
+       with edges = (make-array (length records) :initial-contents records)
+       for pos from 0 below (length edges)
+       when (consp (aref edges pos)) do
+	 (setf (aref edges pos) (labels ((descend (array indexes)
+					   (if indexes
+					       (loop for e in indexes
+						  for val = (aref array e) do
+						    (setf (aref array e) nil)
+						  summing (descend array val))
+					       1)))
+				  (1- (descend edges (aref edges pos)))))
+       finally (return (values (aref edges 0) (loop for edge across edges when edge counting edge))))))
