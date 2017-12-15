78c6014199b1e2ea96cc613d11ddfe92281d6c34
Eugene Zaikonnikov <eugene@funcall.org>
2017-12-15 10:58:48 +0100

    day 15

--- 15.lisp
+++ 15.lisp
@@ -2,8 +2,7 @@
 
 (defun make-generator (state factor divisor)
   #'(lambda ()
-      (loop for st = (setf state (rem (* state factor) 2147483647)) until (zerop (rem st divisor)))
-      state))
+      (do () ((zerop (rem (setf state (rem (* state factor) 2147483647)) divisor)) state))))
 
 (defun generators (salt1 salt2 &optional (count 5000000) (div1 4) (div2 8))
   (loop with gen-a = (make-generator salt1 16807 div1) and gen-b = (make-generator salt2 48271 div2)
